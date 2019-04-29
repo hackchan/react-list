e8fc60542c184a2159fb6d567798fbce093be8f1
root <root@pc0000015487.ac75.com>
2019-04-29 18:17:45 -0500

    se adiciona el boton add

--- src/App.js
+++ src/App.js
@@ -15,8 +15,27 @@ class App extends React.Component{
     }
   }
 
-  updateNewtextValue = (event) => {
-    this.setState({newItemtext:event.target.value})
+  createNewTodo = () =>{
+    
+    if(!this.state.todoItems.find((item )=> {
+        return item.action == this.state.newItemText})){
+          this.setState({
+             todoItems:[...this.state.todoItems,
+              {action:this.state.newItemText, donde:false}],newItemText:""
+
+          })
+  
+        }
+    
+    
+
+ 
+  
+  }
+
+  updateNewTextValue = (event) => {
+    console.log(event.target.value)
+    this.setState({newItemText:event.target.value})
   }
   changeStateData = () => {
     this.setState({
@@ -28,10 +47,20 @@ class App extends React.Component{
     <div>
       <h4 className="bg-primary text-white text-center p-2">
        {this.state.userName}'s To Do List
+       ({this.state.todoItems.filter( t => !t.done).length} items to do)
       </h4>
-      <button onClick={this.changeStateData} className="btn btn-primary m-2">
-       Change
-      </button>
+      
+      <div className="container-fluid">
+           <div className="my-1">
+               <input className="form-control"
+                 value={this.state.newItemText}
+                 onChange={this.updateNewTextValue}
+                />
+                 <button className="btn btn-primary mt-1" 
+                 onClick={this.createNewTodo}>Add
+                </button>       
+           </div>
+      </div>
     </div>
   );
   }
